9e7851c4233ad97baa082df1d660d6789765943c
Pixelbytes <127681129+BoxThatBoxed@users.noreply.github.com>
2023-12-06 18:06:25 -0600

    day 3 of learning python
    Here is day 3 of learning python using BroCode's Turotrial

---
+++ main.py
@@ -0,0 +1,20 @@
+#PS: I already know about the print function thats why i skipped it
+
+#Variables String (str)
+
+#String python script
+#first_name = "Cheese"
+#last_name ="McSwiss"
+#full_name = first_name +" "+last_name
+#print ("Hello "+full_name)
+
+#Integer (int) data type:
+
+#age = 9
+#age = age + 1
+#print ("Your age is:"+str(age))
+#print (type(age))
+
+#float = floating point number (its just a decimal lol)
+height = 146.6
+print ("Your height is: "+str(height)+"cm")
